3bd75849aaaf7ad476d1c2c042b8b29d93ba8c9e
Miguel Fernandez <miguel.fernandez@codeimmersives.com>
2019-04-29 21:04:39 -0400

    Added data.js

---
+++ data.js
@@ -0,0 +1,148 @@
+const data = {
+    "profile" : [
+    {
+        "name": "Miguel A. Fernandez",
+        "address": "43 Bella Vista Avenue",
+        "city": "Saddle Brook, NJ 07663"
+    },
+    {
+        "email": "mferna@outlook.com",
+        "github": "https://github.com/mif9075",
+        "phone": "347-453-6989"
+    },
+    {
+        "objective": "",  
+        "profile": "",
+        "summary": "Web developer student at Code Immersives.",
+        "summary2": "Experienced in HTML, CSS, Flexbox, Gridbox and Javascript."
+    },
+    {
+        "organization": "ASCP",
+        "title": "Certified Laboratory Technologist"
+    },
+    {
+        "language": ["English", "Spanish"]
+    },
+    {
+        "skill": "CSS",
+        "skill2": "HTML",
+        "skill3": "Javascript"
+    }
+    ],
+
+    "experience" : [
+    {
+        "employer": "New York Presbyterian Hospital",
+        "title": "Blood Bank",
+        "year": "September 2014 - Present",
+        "address": "New York, NY",
+        "responsibilities": "",
+        "achievements": ""
+    },
+    {
+        "employer": "North Shore University Hospital",
+        "title": "Blood Bank",
+        "year": "February 2012 - August 2014",
+        "address": "Manhasset, NY",
+        "responsibilities": "",
+        "achievements": ""
+    }
+    ],
+
+    "education" : [
+    {
+        "address": "630 Ninth Ave, Ste 901, New York, NY 10036",
+        "degree": "Web Development Certificate",
+        "school": "Code Immersives",
+        "year": "January 2019 - December 2019"
+    },
+    {
+        "address": "Cincinnati, Ohio",
+        "degree": "Bachelor of Science in Clinical Laboratory Science",
+        "school": "University of Cincinnati",    
+        "year": "2006-2011"
+    }
+    ],
+
+    "project" : [
+    {
+        "name": "The Maze of Methodical Monster",
+        "date": "4/3/2019",
+        "description": "This project is a game-making project, where player and monster take damage.",
+        "website": "https://github.com/mif9075/the-maze-of-methodical-monsters"
+    },
+    {
+        "name": "Portfolio",
+        "date": "4/2/2019",
+        "description": "Portfolio of projects, work experience and education.",
+        "website": "https://github.com/mif9075/portfolio"
+    },
+    {
+        "name": "Boston Red Sox Book",
+        "date": "3/27/2019",
+        "description": "Boston Red Sox lineup using objects in Javascript.",
+        "website": "https://github.com/mif9075/Bostonredsoxbook"
+    },
+    {
+        "name": "ToDone: Object",
+        "date": "3/17/2019",
+        "description": "ToDone: Object Version.",
+        "website": "https://github.com/mif9075/todone-object-version"
+    },
+    {
+        "name": "ToDone",
+        "date": "3/18/2019",
+        "description": "ToDone.",
+        "website": "https://github.com/mif9075/todone"
+    },
+    {
+        "name": "Just How We roll",
+        "date": "3/21/2019",
+        "description": "Random dice project.",
+        "website": "https://github.com/mif9075/just-how-we-roll"
+    },
+    {
+        "name": "2do",
+        "date": "3/14/2019",
+        "description": "To do list with completed and uncompleted lists.",
+        "website": "https://github.com/mif9075/2do"
+    },
+    {
+        "name": "Bucket List",
+        "date": "3/5/2019",
+        "description": "Extremely simple todo app.",
+        "website": "https://github.com/mif9075/bucket-list"
+    },
+    {
+        "name": "Multicalc",
+        "date": "3/8/2019",
+        "description": "Calculator to be able to perform the same operation on multiple numbers at once.",
+        "website": "https://github.com/mif9075/multicalc"
+    },
+    {
+        "name": "the Input Game",
+        "date": "2/27/2019",
+        "description": "Look at five inputs from the user and give them a result. It will do so by comparing the input they typed to a range of possible answers.",
+        "website": "https://github.com/mif9075/the-input-game"
+    },
+    {
+        "name": "Basic Javascript Page",
+        "date": "2/20/2019",
+        "description": "Introduction to Javascript.",
+        "website": "https://github.com/mif9075/basic-js-page"
+    },
+    {
+        "name": "Boostrap Title Case",
+        "date": "2/20/2019",
+        "description": "Bootstrap-layouted app.",
+        "website": "https://github.com/mif9075/bootstrap-title-case"
+
+    },
+    {
+        "name": "Velcro",
+        "date": "2/14/2019",
+        "description": "An exercise in which you will re-implement Bootstrap-s grid system.",
+        "website":"https://github.com/mif9075/velcro"
+    }
+    ]
+}
